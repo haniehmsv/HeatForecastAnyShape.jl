bb924ebb2c0f216bfb4baab72cf0cbe806aa4773
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2023-10-05 15:14:21 -0700

    added nthreads_max=1 to constructGrids

--- src/temperature/grids.jl
+++ src/temperature/grids.jl
@@ -24,7 +24,7 @@ end
 function constructGrids(Δx,bounds;Ntheta=500,nthreads_max=1)
     xlim = bounds[1]
     ylim = bounds[2]
-    g = PhysicalGrid(xlim,ylim,Δx,nthreads_max)
+    g = PhysicalGrid(xlim,ylim,Δx;nthreads_max)
     cache = SurfaceScalarCache(g)
     CacheType = typeof(cache)
     constructGrids{typeof(xlim), CacheType}(Δx,xlim,ylim,g,cache,Ntheta)
