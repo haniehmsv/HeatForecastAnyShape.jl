8c58dadd695278c0fa0d3bea6f693dbd60518352
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2023-10-30 16:26:07 -0700

    added linear interpolation for the top surface

--- src/temperature/temperature_solution.jl
+++ src/temperature/temperature_solution.jl
@@ -256,7 +256,7 @@ function TemperatureSolution(x::AbstractVector,gridConfig::constructGrids,obs::T
 	#x_node = collect(pts.u)
     #y_node = collect(pts.v)
     #spl = Spline2D(x_node, y_node, collect(Ts); kx=5, ky=5, s=0.2)
-    Tfield = get_linear_interpolate(prob,sys,Ts)
+    Tfield = get_linear_interpolate(prob,sys,collect(Ts))
     T_sens .= [Tfield(real(sens[j])) for j in 1:Ny]
 	#T_sens .= [evaluate(spl, real(sens[j]), imag(sens[j])) for j in 1:Ny]
     return T_sens
